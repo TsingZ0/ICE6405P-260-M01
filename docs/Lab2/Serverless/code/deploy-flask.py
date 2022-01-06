1afbd036349b303b3b9da7a0541c9765f7cdc7b7
davidliyutong <davidliyutong@sjtu.edu.cn>
2022-01-06 19:03:29 +0800

    Change conda+pytorch to pip+onnx

--- docs/Lab2/Serverless/code/deploy-flask.py
+++ docs/Lab2/Serverless/code/deploy-flask.py
@@ -1,27 +1,21 @@
-import torch
 from flask import Flask, jsonify, request, Response
-from typing import Callable, Dict, Union
+from typing import Dict, Union
 from io import BytesIO
 from PIL import Image
 import urllib.request
-from torchvision import transforms
-from model import LeNet5
 from gevent import pywsgi
 import os
+import onnx
+import onnxruntime as ort
+import numpy as np
 
 app = Flask(__name__)
 
-net: torch.nn.Module = None
+ort_session: ort.InferenceSession = None
 
-DEFAULT_TRANSFORM: Callable = transforms.Compose(
-    [transforms.Grayscale(),
-     transforms.Resize(28),
-     transforms.ToTensor()])
+PATH_TO_ONNX_MODEL = os.environ["PATH_TO_ONNX_MODEL"]
 
-PATH_TO_STATE_DICT = os.environ["PATH_TO_STATE_DICT"]
-
-
-def decode_picture_from_url(url: str) -> Union[None, torch.Tensor]:
+def decode_picture_from_url(url: str) -> Union[None, np.array]:
     """Decode a picture from OSS service
     e.g. http://192.168.1.82:9000/mnist/082d97b2-19f1-11ec-a558-1e00d10c4441.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=testAccessKey%2F20210921%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210921T165246Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=00acbe0487c7dab233d75ec64b3385fccb26dd7c6c8a83858490bdc1e002280e
 
@@ -45,8 +39,9 @@ def decode_picture_from_url(url: str) -> Union[None, torch.Tensor]:
 
     try:   
         img = Image.open(BytesIO(urllib.request.urlopen(url, timeout=10).read()))
-        img = DEFAULT_TRANSFORM(img)
-        return img.unsqueeze(0)
+        img = img.resize((28, 28), Image.ANTIALIAS).convert('L')
+        img_np = np.array(img)
+        return np.expand_dims(np.expand_dims(img_np, axis=0), axis=0).astype(np.float32) / 255
     except Exception as err:
         print(err)
         return None
@@ -67,10 +62,10 @@ def infer():
     Returns:
         [type]: [description]
     """
-    global net
-    if net is None: # Neural Network not inited
+    global ort_session
+    if ort_session is None: # Neural Network not inited
         init()
-        if net is None:
+        if ort_session is None:
             print("[ Error ] Failed to init neural network:")
             return jsonify({"code": 500, "res": -3})
 
@@ -84,12 +79,14 @@ def infer():
 
     stimulis = decode_picture_from_url(obj_info["url"])
     if stimulis is not None:
-        pred = net(stimulis)
-        pred_decoded = torch.argmax(pred, dim=1)
+        
+        stimulis = {ort_session.get_inputs()[0].name:stimulis}
+        pred = ort_session.run(None, stimulis)[0]
+        res = np.argmax(pred[0])
+
         print("[ Info ] Prediction tensor is:", pred)
-        res = int(pred_decoded.cpu().numpy())
         print("[ Info ] Prediction decoded is:", res)
-        return jsonify({"code": 200, "res": res})
+        return jsonify({"code": 200, "res": int(res)})
     else:
         print("[ Error ] stimulis is None:")
         return jsonify({"code": 500, "res": -1})
@@ -98,24 +95,21 @@ def infer():
 def init():
     """Prepare the neural network
     """
-    global net
+    global ort_session
 
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-    else:
-        device = torch.device('cpu')
+    if ort_session is None:
+        model = onnx.load(PATH_TO_ONNX_MODEL)
+        onnx.checker.check_model(model)
+        ort_session = ort.InferenceSession(PATH_TO_ONNX_MODEL)
 
-    net = LeNet5()
-    net.load_state_dict(torch.load(PATH_TO_STATE_DICT))
-    net.to(device)
-    net.eval()
     return Response("OK", status=200, mimetype='text/html')
 
 
 if __name__ == '__main__':
     SERVING_PORT: int = 8080
     init()
+    # app.run()
     server = pywsgi.WSGIServer(('0.0.0.0', SERVING_PORT), app)
     server.serve_forever()
-    # curl -X POST -d '{"value":{"url":"http://192.168.1.131:9000/mnist/082d97b2-19f1-11ec-a558-1e00d10c4441.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=testAccessKey%2F20210921%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210921T165246Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=00acbe0487c7dab233d75ec64b3385fccb26dd7c6c8a83858490bdc1e002280e"}}' -H 'Content-Type: application/json' http://localhost:8080/run
+    # curl -X POST -d '{"value":{"url":"http://192.168.50.169:9000/mnist/test_picture.png"}}' -H 'Content-Type: application/json' http://localhost:8080/run
     # curl -X POST http://localhost:8080/init
