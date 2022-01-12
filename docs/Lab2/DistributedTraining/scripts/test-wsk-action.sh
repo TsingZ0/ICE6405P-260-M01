11cf9004a26777f70dde3c2f4d2fceca2a1a1fcd
davidliyutong <davidliyutong@sjtu.edu.cn>
2022-01-13 01:17:56 +0800

    Add +x permission to scripts

--- docs/Lab2/DistributedTraining/scripts/test-wsk-action.sh
+++ docs/Lab2/DistributedTraining/scripts/test-wsk-action.sh
@@ -1,9 +1,49 @@
 SERVER_HOST=http://172.17.0.1:29500 
-DATASET_URL=http://172.17.0.1:9000/mnist-dataset/dataset_dl_1.tar.gz
+DATASET_HOST=http://172.17.0.1:9000
 wsk action invoke dist-train \
     --param batch_sz_train 32 \
     --param epoch_n 8 \
     --param apihost $SERVER_HOST \
     --param update_intv 8 \
-    --param dataset_url $DATASET_URL \
+    --param dataset_url $DATASET_HOST/mnist-dataset/dataset_dl_1.tar.gz \
+    --param device cpu
+
+wsk action invoke dist-train \
+    --param batch_sz_train 32 \
+    --param epoch_n 8 \
+    --param apihost $SERVER_HOST \
+    --param update_intv 8 \
+    --param dataset_url $DATASET_HOST/mnist-dataset/dataset_dl_2.tar.gz \
+    --param device cpu
+
+wsk action invoke dist-train \
+    --param batch_sz_train 32 \
+    --param epoch_n 8 \
+    --param apihost $SERVER_HOST \
+    --param update_intv 8 \
+    --param dataset_url $DATASET_HOST/mnist-dataset/dataset_dl_3.tar.gz \
+    --param device cpu
+
+wsk action invoke dist-train \
+    --param batch_sz_train 32 \
+    --param epoch_n 8 \
+    --param apihost $SERVER_HOST \
+    --param update_intv 8 \
+    --param dataset_url $DATASET_HOST/mnist-dataset/dataset_dl_4.tar.gz \
+    --param device cpu
+
+wsk action invoke dist-train \
+    --param batch_sz_train 32 \
+    --param epoch_n 8 \
+    --param apihost $SERVER_HOST \
+    --param update_intv 8 \
+    --param dataset_url $DATASET_HOST/mnist-dataset/dataset_dl_5.tar.gz \
+    --param device cpu
+
+wsk action invoke dist-train \
+    --param batch_sz_train 32 \
+    --param epoch_n 8 \
+    --param apihost $SERVER_HOST \
+    --param update_intv 8 \
+    --param dataset_url $DATASET_HOST/mnist-dataset/dataset_dl_6.tar.gz \
     --param device cpu
